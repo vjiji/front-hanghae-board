0f59c62f4d22e99b9ccb05323c1fbef2cf69cda5
함석원 <s1@hamseog-won-ui-Macmini.local>
2024-03-13 20:07:38 +0900

    feat: main page 작업중

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,70 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Main = () => {
   return (
-    <div>
-      <h1>건들건들</h1>
-    </div>
+    <ArticleMain>
+      <ArticleMain>
+        <div>
+          <div>
+            <h2>
+              &#34;항햐99 과연 이대로
+              괜찮은가...&#34;
+            </h2>
+            <div>
+              <span>이은미 기자</span>
+              <span>88,000</span>
+            </div>
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h3>
+            &#34;최원장 튜터님 ㅅㄹ해요...&#34;
+          </h3>
+          <div>
+            <span>김선하 기자</span>
+            <span>88,000</span>
+          </div>
+        </div>
+      </ArticleMain>
+      <ArticleList>
+        <ul>
+          <li>
+            <p>요즘 개발자 어떤가요?</p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </li>
+          <li>
+            <p>닌텐도 스위치 너무 재미있다...</p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </li>
+          <li>
+            <p>278 아델루나</p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </li>
+          <li>
+            <p>대한민국 축구 앞으로...</p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </li>
+        </ul>
+      </ArticleList>
+    </ArticleMain>
   );
 };
+const ArticleMain = styled.div``
+const ArticleList = styled.div``
 
 export default Main;
