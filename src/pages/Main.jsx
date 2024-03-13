45ba655e2e4f31ce7447f6f9ac222242b95ff977
kim seonha <seon7624@naver.com>
2024-03-13 11:34:45 +0900

    fix: test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Main = () => {
-  return <div>test</div>;
+  return (
+    <>
+      <div>test</div>
+      <h1>충돌이 일어나나요?</h1>
+    </>
+  );
 };
 
 export default Main;
