71e5f79fd0a57b0f36c49c351d458289def07d5a
함석원 <s1@hamseog-won-ui-Macmini.local>
2024-03-13 22:55:32 +0900

    fix: main 작업중

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,48 +5,75 @@ const Main = () => {
   return (
     <MainArticle>
       <ArticleTop>
-        <div>
-          <div>
+        <BigArticle>
+          <InfoWrap>
             <h2>
               &#34;항햐99 과연 이대로
               괜찮은가...&#34;
             </h2>
-            <div>
+            <Editor>
               <span>이은미 기자</span>
               <span>88,000</span>
-            </div>
-          </div>
-          <picture>
+            </Editor>
+          </InfoWrap>
+          <ImgWrap>
             <img src="" alt="" />
-          </picture>
-        </div>
-        <div>
-          <picture>
+          </ImgWrap>
+        </BigArticle>
+        <MiniArticle>
+          <ImgWrap>
             <img src="" alt="" />
-          </picture>
-          <h3>
-            &#34;최원장 튜터님 ㅅㄹ해요...&#34;
-          </h3>
-          <div>
-            <span>김선하 기자</span>
-            <span>88,000</span>
-          </div>
-        </div>
+          </ImgWrap>
+          <InfoWrap>
+            <h3>
+              &#34;최원장 튜터님 ㅅㄹ해요...&#34;
+            </h3>
+            <Editor>
+              <span>김선하 기자</span>
+              <span>88,000</span>
+            </Editor>
+          </InfoWrap>
+        </MiniArticle>
       </ArticleTop>
-      <ArticleBottom>
+      <ArticleMainList>
         <ul>
           <li>
             <p>요즘 개발자 어떤가요?</p>
-            <div>
+            <ImgWrap>
               <img src="" alt="" />
-            </div>
+            </ImgWrap>
           </li>
         </ul>
-      </ArticleBottom>
+      </ArticleMainList>
     </MainArticle>
   );
 };
-const ArticleMain = styled.div``;
-const ArticleList = styled.div``;
+const MainArticle = styled.div``;
+const ArticleTop = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  h2 {
+    font-size: 36px;
+    font-weight: 800;
+  }
+`;
+const BigArticle = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+`;
+const ArticleMainList = styled.div``;
+const MiniArticle = styled.div``;
+const InfoWrap = styled.div``;
+const ImgWrap = styled.picture`
+  img {
+    width: 100%;
+  }
+`;
+const Editor = styled.picture`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  color: #999;
+`;
 
 export default Main;
