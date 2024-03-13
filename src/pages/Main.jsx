955b4d8ccd495f98b1d4599c2628fadcd038828b
함석원 <s1@hamseog-won-ui-Macmini.local>
2024-03-13 20:59:38 +0900

    fix: main 수정중

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 const Main = () => {
   return (
-    <ArticleMain>
-      <ArticleMain>
+    <MainArticle>
+      <ArticleTop>
         <div>
           <div>
             <h2>
@@ -16,14 +16,14 @@ const Main = () => {
               <span>88,000</span>
             </div>
           </div>
-          <div>
+          <picture>
             <img src="" alt="" />
-          </div>
+          </picture>
         </div>
         <div>
-          <div>
+          <picture>
             <img src="" alt="" />
-          </div>
+          </picture>
           <h3>
             &#34;최원장 튜터님 ㅅㄹ해요...&#34;
           </h3>
@@ -32,8 +32,8 @@ const Main = () => {
             <span>88,000</span>
           </div>
         </div>
-      </ArticleMain>
-      <ArticleList>
+      </ArticleTop>
+      <ArticleBottom>
         <ul>
           <li>
             <p>요즘 개발자 어떤가요?</p>
@@ -41,30 +41,12 @@ const Main = () => {
               <img src="" alt="" />
             </div>
           </li>
-          <li>
-            <p>닌텐도 스위치 너무 재미있다...</p>
-            <div>
-              <img src="" alt="" />
-            </div>
-          </li>
-          <li>
-            <p>278 아델루나</p>
-            <div>
-              <img src="" alt="" />
-            </div>
-          </li>
-          <li>
-            <p>대한민국 축구 앞으로...</p>
-            <div>
-              <img src="" alt="" />
-            </div>
-          </li>
         </ul>
-      </ArticleList>
-    </ArticleMain>
+      </ArticleBottom>
+    </MainArticle>
   );
 };
-const ArticleMain = styled.div``
-const ArticleList = styled.div``
+const ArticleMain = styled.div``;
+const ArticleList = styled.div``;
 
 export default Main;
