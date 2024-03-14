b86caf6ca2378b4fca19e947e71ba2bf6d1ccfb9
vjiji <restmi0711@gmail.com>
2024-03-14 18:27:33 +0900

    feat: 새로운 포스트 작성 api기능을 작성한다

--- src/apis/postsAPI.js
+++ src/apis/postsAPI.js
@@ -2,7 +2,7 @@ const { baseURL } = require('./instance');
 
 const postsAPI = {
   createPost: (post) =>
-    baseURL.post('/posts', post),
+    baseURL.post('/api/posts', post),
 };
 
 export default postsAPI;
