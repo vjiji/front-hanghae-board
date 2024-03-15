067d8892e90886fc7f687f0b372c7ee036bded3f
vjiji <restmi0711@gmail.com>
2024-03-16 01:27:30 +0900

    fix: comment의 create comment 타입 변경 및 updatePost 메서드 변경

--- src/apis/postsAPI.js
+++ src/apis/postsAPI.js
@@ -6,7 +6,7 @@ const postsAPI = {
   getPostDetail: (postId) =>
     baseURL.get(`/api/posts/${postId}`),
   updatePost: (id, post) =>
-    baseURL.put(`/api/posts/${id}`, post),
+    baseURL.post(`/api/posts/${id}`, post),
   // axios put 메서드 안될 경우 fetch
   // fetch(
   //   `${process.env.REACT_APP_SERVER_BASE_URL}/api/posts/${id}`,
