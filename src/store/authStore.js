056ab570fd69c984118680bc64c70e71674c5f50
kim seonha <seon7624@naver.com>
2024-03-16 20:03:40 +0900

    fix: login Zustand 수정

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,15 +1,44 @@
-import create from 'zustand';
+import { create } from 'zustand';
+import {
+  setCookie,
+  getCookie,
+  removeCookie,
+} from '../cookies/cookies';
 
 const useAuthStore = create((set) => ({
-  //초기 상태
-  isLogIn: false,
-  token: null,
-  //로그인 상태 설정
-  login: (token) =>
-    set(() => ({ isLogIn: true, token })),
-  //로그아웃
-  logout: () =>
-    set(() => ({ isLogIn: false, token: null })),
+  // 초기 상태
+  isLogIn: getCookie('token') ? true : false,
+  token: getCookie('token'),
+  userId: null,
+  isReporter: null,
+  email: null,
+  nickname: null,
+  // 로그인 상태
+  login: (
+    token,
+    userId,
+    isReporter,
+    email,
+    nickname,
+  ) => {
+    setCookie('token', token, {});
+    set(() => ({
+      userId,
+      isReporter,
+      email,
+      nickname,
+    }));
+  },
+  // 로그아웃 상태
+  logout: () => {
+    removeCookie('token', {});
+    set(() => ({
+      userId: null,
+      isReporter: null,
+      email: null,
+      nickname: null,
+    }));
+  },
 }));
 
 export default useAuthStore;
