ca66724467c31d1a5ae1891161948952d9eb7655
vjiji <restmi0711@gmail.com>
2024-03-18 15:18:44 +0900

    fix: 로그인 시 토큰을 쿠키에 저장

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -6,13 +6,13 @@ export const baseURL = axios.create({
 });
 
 baseURL.interceptors.request.use((config) => {
-  // const token = getCookie('token');
-  // if (token) {
-  //   config.headers['Authorization'] =
-  //     `Bearer ${token}`;
-  // }
+  const token = getCookie('token');
+  if (token) {
+    config.headers['Authorization'] =
+      `Bearer ${token}`;
+  }
   // 이거 살려서 로그인 회원가입 로그아웃 진행하고 기능 잘 되는지 확인!
-  config.headers['Authorization'] =
-    `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicmFuZHkwMTA4QG5hdmVyLmNvbSIsImF1dGgiOiJVU0VSIiwiZXhwIjoxNzEwOTYxODM0LCJpYXQiOjE3MTA2MDE4MzR9.9Xw7S-1pyCKLYj-kSW9lmIh3KqeHZ6AVtAAM5eRdfEE`;
+  // config.headers['Authorization'] =
+  //   `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicmFuZHkwMTA4QG5hdmVyLmNvbSIsImF1dGgiOiJVU0VSIiwiZXhwIjoxNzEwOTYxODM0LCJpYXQiOjE3MTA2MDE4MzR9.9Xw7S-1pyCKLYj-kSW9lmIh3KqeHZ6AVtAAM5eRdfEE`;
   return config;
 });
